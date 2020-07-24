64755f52dc434734bb5277f2ba264ccb9b5b149e
Deepak G <deepak.g@krds.fr>
2020-07-24 14:29:37 +0530

    s3-storage

--- learning/settings.py
+++ learning/settings.py
@@ -194,3 +194,14 @@ LOGGING={
         }
     }
 }
+
+# AWS secret
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_BUCKET_NAME')
+AWS_S3_REGION_NAME = env('AWS_REGION')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_DEFAULT_ACL = 'public-read'
+
+# set default file storage
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
